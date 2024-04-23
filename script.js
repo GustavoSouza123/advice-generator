455e130d98593a9ae5fcdb2d8ad1a6dce302a6b2
Gustavo Souza <gustavoelia7@gmail.com>
2024-04-23 12:24:21 -0300

    feat: responsiveness

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const adviceIdSpan = document.querySelector('.advice .id span');
 const adviceText = document.querySelector('.advice .text');
 const button = document.querySelector('.advice .button');
 
+/* get advice from Advice Slip JSON API */
+
 const generateAdvice = (url, options = {}) => {
     fetch(url, options)
         .then(res => res.json())
@@ -16,4 +18,14 @@ const generateAdvice = (url, options = {}) => {
 const apiUrl = 'https://api.adviceslip.com/advice';
 
 generateAdvice(apiUrl);
-button.addEventListener('click', () => { generateAdvice(apiUrl) })
+button.addEventListener('click', () => { generateAdvice(apiUrl) });
+
+/* responsiveness */
+
+window.addEventListener('resize', () => {
+    if(this.innerWidth <= 375) {
+        document.querySelector('.advice > img').src = './images/pattern-divider-mobile.svg';
+    } else {
+        document.querySelector('.advice > img').src = './images/pattern-divider-desktop.svg';
+    }
+})
